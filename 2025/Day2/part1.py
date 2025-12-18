197dc69bc6b7ca44dbb2894f925eb512cbdc3890
Chill Riddler <chillriddler@gmail.com>
2025-12-18 12:24:11 -0500

    Day2

---
+++ 2025/Day2/part1.py
@@ -0,0 +1,23 @@
+def patternCheck(my_int):
+    total = 0
+    int_str = str(my_int)
+    if len(int_str)%2:
+        return 0
+    pattern = int_str[:int(len(int_str)/2)]
+    times_repeated = int_str.count(pattern)
+    if times_repeated*len(pattern) == len(int_str):
+        print(f"{my_int} is a repeat of {pattern}")
+        total += my_int
+    return total
+
+
+with open("2025/Day2/input.txt", "r") as file:
+    ID_list = file.read()
+    ID_list = ID_list.split(",")
+
+total = 0
+for ID_range in ID_list:
+    start_point,end_point = ID_range.split("-")
+    for i in range(int(start_point),int(end_point)+1):
+        total += patternCheck(i)
+print(total)
---
+++ 2025/Day2/part2.py
@@ -0,0 +1,22 @@
+def patternCheck(my_int):
+    int_str = str(my_int)
+    for i in range(round(len(int_str)/2)):
+        pattern = int_str[:i+1]
+        times_repeated = int_str.count(pattern)
+        if times_repeated*len(pattern) == len(int_str):
+            print(f"{my_int} is a repeat of {pattern}")
+            return True
+    return False
+
+
+with open("2025/Day2/input.txt", "r") as file:
+    ID_list = file.read()
+    ID_list = ID_list.split(",")
+
+total = 0
+for ID_range in ID_list:
+    start_point,end_point = ID_range.split("-")
+    for i in range(int(start_point),int(end_point)+1):
+        if patternCheck(i):
+            total += i
+print(total)
