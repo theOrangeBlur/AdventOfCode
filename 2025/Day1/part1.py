f0f476221d0caf2299591d7f2477287ed761c7fb
Chill Riddler <chillriddler@gmail.com>
2025-12-18 09:43:31 -0500

    Day1

---
+++ 2025/Day1/part1.py
@@ -0,0 +1,19 @@
+with open("2025\Day1\input.txt", "r") as file:
+    directions = file.read()
+    directions = directions.split("\n")
+
+loc = 50
+total = 0
+for direction in directions:
+    if direction[0] == 'R':
+        loc += int(direction[1:])
+        while loc > 99:
+            loc-=100
+    else:
+        loc -= int(direction[1:])
+        while loc < 0:
+            loc+=100
+
+    if loc == 0:
+        total+=1
+print(total)
---
+++ 2025/Day1/part2.py
@@ -0,0 +1,22 @@
+with open("2025/Day1/input.txt", "r") as file:
+    directions = file.read()
+    directions = directions.split("\n")
+
+loc = 50
+total = 0
+for direction in directions:
+    if direction[0] == "R":
+        iterator = 1
+    else:
+        iterator = -1
+    spaces_list = [loc]
+    for i in range(int(direction[1:])):
+        new_loc = loc + iterator
+        if new_loc == 100:
+            new_loc = 0
+        if new_loc == -1:
+            new_loc = 99
+        spaces_list.append(new_loc)
+        loc = new_loc
+    total += spaces_list[1:].count(0)
+print(total)
